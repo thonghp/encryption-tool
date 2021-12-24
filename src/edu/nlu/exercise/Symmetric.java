92ad4c5684636d62f980e80a024c5e2ef5ccc129
thonghp <thonghoangpham123@gmail.com>
2021-12-24 20:55:30 +0700

    update gcm

--- src/edu/nlu/exercise/Symmetric.java
+++ src/edu/nlu/exercise/Symmetric.java
@@ -3,6 +3,7 @@ package edu.nlu.exercise;
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
+import javax.crypto.spec.GCMParameterSpec;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.*;
@@ -41,18 +42,20 @@ public class Symmetric {
     }
 
     public Cipher doLengthHandling(String original, String inputAlgorithm, String algorithmName) throws Exception {
-        // xử lý đội dài đầu vào với NoPadding
-        String[] split = inputAlgorithm.split("/");
-        String padding = split[1];
+        if (!inputAlgorithm.equals(algorithmName)) {
+            // xử lý đội dài đầu vào với NoPadding
+            String[] split = inputAlgorithm.split("/");
+            String padding = split[1];
 
-        if (inputAlgorithm.contains("NoPadding")) {
-            if ((inputAlgorithm.contains("ECB") || inputAlgorithm.contains("CBC") || inputAlgorithm.contains("PCBC"))
-                    && original.length() % cipher.getBlockSize() != 0) {
-                cipher = Cipher.getInstance(algorithmName + "/" + padding + "/PKCS5Padding");
-            }
+            if (inputAlgorithm.contains("NoPadding") && !algorithmName.equals("ARCFOUR")) {
+                if ((inputAlgorithm.contains("ECB") || inputAlgorithm.contains("CBC") || inputAlgorithm.contains("PCBC"))
+                        && original.length() % cipher.getBlockSize() != 0) {
+                    cipher = Cipher.getInstance(algorithmName + "/" + padding + "/PKCS5Padding");
+                }
 
-            if (inputAlgorithm.contains("CTS") && original.length() < cipher.getBlockSize()) {
-                cipher = Cipher.getInstance(algorithmName + "/CBC/PKCS5Padding");
+                if (inputAlgorithm.contains("CTS") && original.length() < cipher.getBlockSize()) {
+                    cipher = Cipher.getInstance(algorithmName + "/CBC/PKCS5Padding");
+                }
             }
         }
 
@@ -77,6 +80,8 @@ public class Symmetric {
 
         if (inputAlgorithm.equals(algorithmName) || inputAlgorithm.contains("ECB")) {
             cipher.init(Cipher.DECRYPT_MODE, key);
+        } else if (inputAlgorithm.equals("AES/GCM/NoPadding")) {
+            cipher.init(Cipher.DECRYPT_MODE, key, new GCMParameterSpec(128, cipher.getIV()));
         } else {
             cipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(cipher.getIV()));
         }
@@ -87,7 +92,7 @@ public class Symmetric {
         return new String(plainText);
     }
 
-    public void fileHandling(String inputFile, String outputFile) throws Exception {
+    public String fileHandling(String inputFile, String outputFile) throws Exception {
         File file = new File(inputFile);
         long size = 0;
 
@@ -124,49 +129,51 @@ public class Symmetric {
                 bos.write(outputBytes);
             }
         }
+        return rename;
     }
 
-    public void encryptFile(String fileName, String keyPath, String inputAlgorithm, String algorithmName) throws Exception {
+    public String encryptFile(String fileName, String keyPath, String inputAlgorithm, String algorithmName) throws Exception {
         SecretKey key = readKey(keyPath, algorithmName);
 
         cipher = Cipher.getInstance(inputAlgorithm);
         cipher.init(Cipher.ENCRYPT_MODE, key);
 
-        fileHandling(fileName, "E:\\Encryption Key\\encrypt");
+        return fileHandling(fileName, "E:\\Encryption Key\\encrypt");
     }
 
-    public void decryptFile(String fileEncrypt, String keyPath, String inputAlgorithm, String algorithmName) throws Exception {
+    public String decryptFile(String fileEncrypt, String keyPath, String inputAlgorithm, String algorithmName) throws Exception {
         SecretKey key = readKey(keyPath, algorithmName);
 
         if (inputAlgorithm.equals(algorithmName) || inputAlgorithm.contains("ECB")) {
             cipher.init(Cipher.DECRYPT_MODE, key);
+        } else if (inputAlgorithm.equals("AES/GCM/NoPadding")) {
+            cipher.init(Cipher.DECRYPT_MODE, key, new GCMParameterSpec(128, cipher.getIV()));
         } else {
             cipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(cipher.getIV()));
         }
 
         // viết thêm để xử lý đọc 1 lần
-        fileHandling(fileEncrypt, "E:\\Encryption Key\\decrypt");
+        return fileHandling(fileEncrypt, "E:\\Encryption Key\\decrypt");
     }
 
     public static void main(String[] args) throws Exception {
         Symmetric ins = new Symmetric();
-//        ins.generateKeySymmetric("DES", 56);
+        ins.generateKeySymmetric("AES", 192);
 
         /*
          * nopadding => ecb/cbc/pcbc là 8 - cts > 8 (DES/AES)
          * pkcs5/iso => ctr/cts/gcm(chỉ hỗ trợ cho aes) chỉ chơi dc vs nopadding
          */
-        String input = "thong";
-        String algorithmName = "DES";
-        String inputAlgorithm = "DES/CBC/PKCS5Padding";
+        String input = "thong!@#";
+        String algorithmName = "AES";
+        String inputAlgorithm = "AES/GCM/NoPadding";
 //        String inputAlgorithm = "DES";
 
-//        String encrypt = ins.encrypt(input, "E:\\Encryption Key\\symmetric.key", inputAlgorithm, algorithmName);
-//        System.out.println(encrypt);
-//        System.out.println(ins.decrypt(encrypt, "E:\\Encryption Key\\symmetric.key", inputAlgorithm, algorithmName));
+        String encrypt = ins.encrypt(input, "E:\\Encryption Key\\symmetric.key", inputAlgorithm, algorithmName);
+        System.out.println(encrypt);
+        System.out.println(ins.decrypt(encrypt, "E:\\Encryption Key\\symmetric.key", inputAlgorithm, algorithmName));
 
-//        ins.encryptFile("E:\\System Security\\slide.zip", "E:\\Encryption Key\\symmetric.key", inputAlgorithm, algorithmName);
-//        ins.decryptFile("E:\\Encryption Key\\encrypt.zip", "E:\\Encryption Key\\symmetric.key", inputAlgorithm, algorithmName);
-
+//        ins.encryptFile("E:\\Encryption Key\\a.png", "E:\\Encryption Key\\symmetric.key", inputAlgorithm, algorithmName);
+//        ins.decryptFile("E:\\Encryption Key\\encrypt.png", "E:\\Encryption Key\\symmetric.key", inputAlgorithm, algorithmName);
     }
 }
